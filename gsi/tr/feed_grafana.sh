fd63b338988cf6a759cb0b2ebeb4a1b6dd7bbe2f
Enkhbold Ochirsuren <e.ochirsuren@gsi.de>
2020-09-07 19:33:25 +0200

    feed-grafana: monitor the localmaster WRS (instead of the clock master WRS)

--- gsi/tr/feed_grafana.sh
+++ gsi/tr/feed_grafana.sh
@@ -3,26 +3,11 @@
 # This script is used to launch the push scripts that
 # send GMT monitoring data to Grafana running on the tsl019 host.
 
-N_USER_ARGUMENTS=1
-
-usage() {
-  echo "Usage: $0 grafana_host"
-  echo "where:"
-  echo "  grafana_host   - domain name or IP address of a Grafana host"
-  echo
-}
-
-# Check command line arguments
-if [ $# -ne $N_USER_ARGUMENTS ]; then
-  usage
-  exit 1
-fi
-
-GRAFANA_HOST=$1
+GRAFANA_HOST=tsl019.acc.gsi.de
 
 # Terminate running scripts
 killall push-dm-mon.sh
-killall push-nw-mon.sh
+killall push-prod-lm-mon.sh
 killall push-gw-mon.sh
 killall push-tr-mon.sh
 killall push-diag-mon.sh
@@ -30,7 +15,7 @@ killall push-uni-mon.sh
 
 # Start scripts
 push-dm-mon.sh $GRAFANA_HOST &
-push-nw-mon.sh $GRAFANA_HOST &
+push-prod-lm-mon.sh $GRAFANA_HOST &
 push-gw-mon.sh $GRAFANA_HOST &
 push-tr-mon.sh $GRAFANA_HOST &
 push-diag-mon.sh $GRAFANA_HOST &
